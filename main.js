03bce7d30f055b6774996b3b54ca1416d075fccf
Ksenia Baranova <baranova-ksenia@mail.ru>
2021-08-17 23:06:01 +0300

    completed version

--- main.js
+++ main.js
@@ -1,65 +1,68 @@
-// let deckId;
-// const cardsContainer = document.getElementById("cards");
-// const newDeckBtn = document.getElementById("new-deck");
-// const drawBtn = document.getElementById("draw-cards");
+let deckId;
+let computerScore = 0;
+let userScore = 0;
 
-// function handleClick() {
-//   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//       deckId = data.deck_id;
-//     });
-// }
+const cardsContainer = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawBtn = document.getElementById("draw-cards");
+const header = document.getElementById("header");
+const remainingText = document.getElementById("remaining");
+let computerScoreEl = document.getElementById("computer-score");
+let userScoreEl = document.getElementById("user-score");
 
-// newDeckBtn.addEventListener("click", handleClick);
+function handleClick() {
+  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      remainingText.textContent = `Remaining cards: ${data.remaining}`;
+      deckId = data.deck_id;
+    });
+}
 
-// drawBtn.addEventListener("click", () => {
-//   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       cardsContainer.children[0].innerHTML = `<img src=${data.cards[0].image} class="card"/>`;
-//       cardsContainer.children[1].innerHTML = `<img src=${data.cards[1].image} class="card"/>`;
-//     });
-// });
+newDeckBtn.addEventListener("click", handleClick);
+
+drawBtn.addEventListener("click", () => {
+  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      cardsContainer.children[0].innerHTML = `<img src=${data.cards[0].image} class="card"/>`;
+      cardsContainer.children[1].innerHTML = `<img src=${data.cards[1].image} class="card"/>`;
+
+      remainingText.textContent = `Remaining cards: ${data.remaining}`;
+
+      const winnerText = determineCardWinner(data.cards[0], data.cards[1]);
+      header.textContent = winnerText;
+
+      if (data.remaining === 0) {
+        drawBtn.disabled = true;
+        if (computerScore > userScore) {
+          // display "The computer won the game!"
+          header.textContent = "The computer won the game!";
+        } else if (userScore > computerScore) {
+          // display "You won the game!"
+          header.textContent = "You won the game!";
+        } else {
+          // display "It's a tie game!"
+          header.textContent = "It's a tie game!";
+        }
+      }
+    });
+});
 
 function determineCardWinner(card1, card2) {
-  const valueOptions = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "JACK",
-    "QUEEN",
-    "KING",
-    "ACE",
-  ];
+  const valueOptions = ["2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","ACE"];
   const card1IndexValue = valueOptions.indexOf(card1.value);
   const card2IndexValue = valueOptions.indexOf(card2.value);
 
-  console.log("card1:", card1IndexValue);
-  console.log("card2:", card2IndexValue);
-
   if (card1IndexValue > card2IndexValue) {
-    console.log("Card 1 is wins");
+    computerScore++;
+    computerScoreEl.textContent = `Computer score: ${computerScore}`;
+    return "Computer is wins";
   } else if (card1IndexValue < card2IndexValue) {
-    console.log("Card 2 is wins");
+    userScore++;
+    userScoreEl.textContent = `My score: ${userScore}`;
+    return "You are win";
   } else {
-    console.log("It's a tie!");
+    return "It's a tie!";
   }
 }
-
-const card1Obj = {
-  value: "7",
-};
-
-const card2Obj = {
-  value: "KING",
-};
-
-determineCardWinner(card1Obj, card2Obj);
